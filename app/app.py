2ddb0106d8648bf4d849c60d8fc73810a94662c3
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-03 16:36:00 -0300

    Base app

--- app/app.py
+++ app/app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "hello world"
+        return jsonify({'data': data})
+
+
+if __name__ == '__main__':
+  
+    app.run(debug = True)
