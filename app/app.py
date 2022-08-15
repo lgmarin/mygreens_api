2d384d1664034c9f3be16e1f9a38b942e387584d
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-15 16:15:56 -0300

    Configure SQLAlchemy and Models

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config())
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     TESTING = os.environ.get("APP_ENV", "False")
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
 class DevelopmentConfig(Config):
     pass
