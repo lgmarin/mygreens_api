d351542a6c6d8f9b29fc9253eec03fdf1a619214
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-08-17 15:55:48 -0300

    Initialize database

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from config import configs
+db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(configs[config_name])
+
+    db.init_app(app)
 
     return app
