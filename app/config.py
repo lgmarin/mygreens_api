e0711fa12dfe727d42c05b0a19246773711fbf05
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-08-17 15:01:35 -0300

    Update configuration and gitignore

--- app/config.py
+++ app/config.py
@@ -1,13 +1,31 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    TESTING = os.environ.get("APP_ENV", "False")
+    DEBUG = False
+    TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
 class DevelopmentConfig(Config):
-    pass
-
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_dev.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
 
 class TestingConfig(Config):
+    DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    pass
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
